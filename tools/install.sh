bd8b66ea97b1a7324fd50bacf856a0d56d44342b
Manuel Rüger <manuel@rueg.eu>
2023-01-26 14:01:05 +0100

    tools: Keep list of tools in one place
    This way you only need to add them to tools.go
    
    Co-Authored-By: Elana Hashman <ehashman@apple.com>

--- tools/install.sh
+++ tools/install.sh
@@ -2,13 +2,5 @@
 
 set -e
 
-cd tools
-
-go install \
-    github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway \
-    github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2 \
-    google.golang.org/protobuf/cmd/protoc-gen-go \
-    google.golang.org/grpc/cmd/protoc-gen-go-grpc \
-    sigs.k8s.io/controller-tools/cmd/controller-gen \
-    github.com/golangci/golangci-lint/cmd/golangci-lint \
-    sigs.k8s.io/kustomize/kustomize/v3
+# Tools are recorded as imports in tools.go, this command extracts the import directive, lists the dependencies and installs them
+grep '^\s*_' tools/tools.go | awk '{print $2}' | xargs -t go install -mod=readonly -modfile=tools/go.mod
